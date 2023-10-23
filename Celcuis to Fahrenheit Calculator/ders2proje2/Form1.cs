3ad9325b2eacc30047c9270c2b3ed3c423f0fc9b
MDenizCan <115082673+MDenizCan@users.noreply.github.com>
2023-10-23 23:14:23 +0300

    Add files via upload

---
+++ Celcuis to Fahrenheit Calculator/ders2proje2/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ders2proje2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float CtoFdegree = (float.Parse(textBox1.Text) * 9f / 5f) + 32;
+            textBox2.Text = CtoFdegree.ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            float FtoCdegree = (float.Parse(textBox1.Text) - 32) * 5f / 9f;
+            textBox2.Text = FtoCdegree.ToString();
+        }
+    }
+}
